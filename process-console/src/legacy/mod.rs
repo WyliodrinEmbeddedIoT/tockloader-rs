be9dd25b042ab54ac189e4fdea722b3e0f220b43
addrian-77 <lunguadrian30@gmail.com>
2025-07-22 12:06:37 +0300

    feat(serial): add terminal handling (WIP)
    Signed-off-by: addrian-77 <lunguadrian30@gmail.com>

--- process-console/src/legacy/mod.rs
+++ process-console/src/legacy/mod.rs
@@ -1,4 +1,17 @@
-pub fn run() {
-    // TODO
-    panic!();
+use clap::ArgMatches;
+
+use crate::legacy::errors::TockloaderError;
+use crate::legacy::serial::interface::build_interface;
+use crate::legacy::serial::traits::BoardInterface;
+use crate::legacy::serial::traits::VirtualTerminal;
+
+mod errors;
+mod serial;
+
+pub async fn run(sub_matches: &ArgMatches) -> Result<(), TockloaderError> {
+    let mut interface = build_interface(sub_matches)?;
+    interface.open()?;
+    interface.run_terminal().await?;
+
+    Ok(())
 }
---
+++ process-console/src/legacy/serial/mod.rs
@@ -0,0 +1,5 @@
+pub mod interface;
+mod serial;
+pub mod terminal;
+pub mod traits;
+pub mod board_interface;
