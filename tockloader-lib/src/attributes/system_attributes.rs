af65e1508b8e9ccfacdc424ff9d0100e7ed12c54
Mohamed Daoudo <mohamed.daoudo@stud.fils.upb.ro>
2025-07-28 17:16:08 +0300

    chore: document general and app attributes
    Signed-off-by: Mohamed Daoudo <mohamed.daoudo@stud.fils.upb.ro>

--- tockloader-lib/src/attributes/system_attributes.rs
+++ tockloader-lib/src/attributes/system_attributes.rs
@@ -11,6 +11,13 @@ use crate::errors::TockloaderError;
 
 use super::decode::{bytes_to_string, decode_attribute};
 
+/// This structure contains all relevant information about board that is stored
+/// in the bootloader ROM.
+///
+/// Note: not all system attributes are present on all boards. You cannot assume
+/// any of these structure members are `Some(_)`.
+///
+/// See also <https://book.tockos.org/doc/kernel_attributes.html?highlight=attributes#header-format>
 #[derive(Debug)]
 pub struct SystemAttributes {
     pub board: Option<String>,
@@ -43,28 +50,47 @@ impl SystemAttributes {
         }
     }
 
-    // TODO: explain what is happening here
+    /// Read system attributes using a probe-rs connection. A bootloader must be
+    /// present on this board for this function to work properly.
+    ///
+    /// # Parameters
+    /// - `board_core` : Core access, obtained from a
+    ///   [ProbeRSConnection](crate::connection::ProbeRSConnection)
+    ///
+    /// # Returns
+    /// - Ok(result): if attributes were read successfully
+    /// - Err(TockloaderError::MisconfiguredBoard): if no start address is found or valid
+    /// - Err(TockloaderError::MisconfiguredBoard): if attributes don't follow the UTF-8 format
+    /// - Err(TockloaderError::ProbeRsReadError): if reading fails
     pub(crate) fn read_system_attributes_probe(
         board_core: &mut Core,
     ) -> Result<Self, TockloaderError> {
         let mut result = SystemAttributes::new();
+        // System attributes start at 0x600 and up to 0x9FF. See:
+        // https://book.tockos.org/doc/memory_layout#flash-1
         let address = 0x600;
+        // Each attribute is 64 bytes exactly, and there are 16 slots
         let mut buf = [0u8; 64 * 16];
 
         let _ = board_core.read(address, &mut buf);
 
         let mut data = buf.chunks(64);
 
-        for index_data in 0..data.len() {
-            let step = match data.next() {
+        for current_slot in 0..data.len() {
+            let slot_data = match data.next() {
                 Some(data) => data,
                 None => break,
             };
 
-            let step_option = decode_attribute(step);
-
-            if let Some(decoded_attributes) = step_option {
-                match index_data {
+            // If the attribute chunk was successfully decoded, assign its value
+            // to the corresponding field in `result` based on the index:
+            // - 0 = board name,
+            // - 1 = architecture,
+            // - 2 = application start address (parsed from hex string),
+            // - 3 = boot hash, _ = invalid or missing data is skipped.
+            // NOTE: this can also be done by looping directly through the key attributes.
+            if let Some(decoded_attributes) = decode_attribute(slot_data) {
+                match current_slot {
                     0 => {
                         result.board = Some(decoded_attributes.value.to_string());
                     }
@@ -72,6 +98,7 @@ impl SystemAttributes {
                         result.arch = Some(decoded_attributes.value.to_string());
                     }
                     2 => {
+                        // Parse hex string like "0x40000" into actual u64 value
                         result.appaddr = Some(
                             u64::from_str_radix(
                                 decoded_attributes
@@ -97,8 +124,9 @@ impl SystemAttributes {
             }
         }
 
-        let address = 0x40E;
+        // TODO(george-cosma): separate kernel attributes from kernel flags.
 
+        let address = 0x40E;
         let mut buf = [0u8; 8];
 
         let _ = board_core.read_8(address, &mut buf);
@@ -113,6 +141,7 @@ impl SystemAttributes {
 
         result.bootloader_version = Some(string.to_owned());
 
+        // The 100 bytes prior to the application start address are reserved for the kernel attributes and flags
         let mut kernel_attr_binary = [0u8; 100];
         board_core
             .read(
@@ -142,18 +171,33 @@ impl SystemAttributes {
         Ok(result)
     }
 
-    // TODO: explain what is happening here
+    /// Read system attributes using a serial connection. A bootloader must be
+    /// present on this board for this function to work properly.
+    ///
+    /// # Parameters
+    /// - `port`: Serial access, obtained from a
+    ///   [SerialConnection](crate::connection::SerialConnection)
+    ///
+    /// # Returns
+    /// - Ok(result): if attributes were read successfully
+    /// - Err(TockloaderError::MisconfiguredBoard): if no start address is found or valid
+    /// - Err(TockloaderError::MisconfiguredBoard): if attributes don't follow the UTF-8 format
+    /// - Err(TockloaderError::SerialReadError): if reading fails
     pub(crate) async fn read_system_attributes_serial(
         port: &mut SerialStream,
     ) -> Result<Self, TockloaderError> {
         let mut result = SystemAttributes::new();
 
+        // System attributes start at 0x600 and up to 0x9FF. See:
+        // https://book.tockos.org/doc/memory_layout#flash-1
         let mut pkt = (0x600_u32).to_le_bytes().to_vec();
+        // Each attribute is 64 bytes exactly, and there are 16 slots
         let length = (1024_u16).to_le_bytes().to_vec();
         for i in length {
             pkt.push(i);
         }
 
+        // Read the kernel attributes
         let (_, buf) = issue_command(
             port,
             Command::ReadRange,
@@ -166,16 +210,14 @@ impl SystemAttributes {
 
         let mut data = buf.chunks(64);
 
-        for index_data in 0..data.len() {
-            let step = match data.next() {
+        for current_slot in 0..data.len() {
+            let slot_data = match data.next() {
                 Some(data) => data,
                 None => break,
             };
 
-            let step_option = decode_attribute(step);
-
-            if let Some(decoded_attributes) = step_option {
-                match index_data {
+            if let Some(decoded_attributes) = decode_attribute(slot_data) {
+                match current_slot {
                     0 => {
                         result.board = Some(decoded_attributes.value.to_string());
                     }
@@ -183,6 +225,7 @@ impl SystemAttributes {
                         result.arch = Some(decoded_attributes.value.to_string());
                     }
                     2 => {
+                        // Parse hex string like "0x40000" into actual u64 value
                         result.appaddr = Some(
                             u64::from_str_radix(
                                 decoded_attributes
@@ -208,12 +251,15 @@ impl SystemAttributes {
             }
         }
 
+        // TODO(george-cosma): separate kernel attributes from kernel flags.
+
         let mut pkt = (0x40E_u32).to_le_bytes().to_vec();
         let length = (8_u16).to_le_bytes().to_vec();
         for i in length {
             pkt.push(i);
         }
 
+        // Read bootloader version
         let (_, buf) =
             issue_command(port, Command::ReadRange, pkt, true, 8, Response::ReadRange).await?;
 
@@ -223,20 +269,24 @@ impl SystemAttributes {
             )
         })?;
 
+        // Strip null bytes
         let string = string.trim_matches(char::from(0));
-
         result.bootloader_version = Some(string.to_owned());
 
+        // The 100 bytes prior to the application start address are reserved
+        // for the kernel attributes and flags
         let mut pkt = ((result.appaddr.ok_or(TockloaderError::MisconfiguredBoard(
             "No start address found.".to_owned(),
         ))? - 100) as u32)
             .to_le_bytes()
             .to_vec();
+
         let length = (100_u16).to_le_bytes().to_vec();
         for i in length {
             pkt.push(i);
         }
 
+        // Read kernel flags
         let (_, kernel_attr_binary) = issue_command(
             port,
             Command::ReadRange,
