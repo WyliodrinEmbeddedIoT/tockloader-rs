d0e654f4f0e0b23675ccac1b0e54bd212896512c
Soare Catalin <sunshine640@proton.me>
2026-07-31 11:20:08 +0300

    raw fn's for ConsoleConnection
    Signed-off-by: Soare Catalin <sunshine640@proton.me>

--- tockloader-lib/src/console/console_connection.rs
+++ tockloader-lib/src/console/console_connection.rs
@@ -1,59 +1,83 @@
 use anyhow;
-use bytes::Bytes;
-use tokio::io::{split, AsyncReadExt, AsyncWriteExt};
+use futures::{SinkExt, StreamExt};
+use std::sync::Mutex;
 use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
-use tokio_serial::{SerialPortBuilderExt, SerialStream};
+use tokio_serial::SerialStream;
+use tokio_util::codec::{Decoder, Encoder, Framed};
 
 /// The ConsoleConnection is represented by a pair of channels
 /// a log_reader for receiving data from the board
 /// a command_writer for sending commands to the board
 #[allow(dead_code)]
-pub struct ConsoleConnection {
-    log_reader: UnboundedReceiver<Bytes>,
-    command_writer: UnboundedSender<Bytes>,
-    error_reader: UnboundedReceiver<String>,
+pub struct ConsoleConnection<OutItem, InItem> {
+    pub log_reader: Mutex<UnboundedReceiver<OutItem>>,
+    pub command_writer: UnboundedSender<InItem>,
+    pub error_reader: Mutex<UnboundedReceiver<String>>,
 }
 
-pub async fn init(tty: &str) -> Result<ConsoleConnection, anyhow::Error> {
-    let mut stream: SerialStream = tokio_serial::new(tty, 115200).open_native_async()?;
-
+pub async fn init<C, InItem>(
+    mut stream: SerialStream,
+    codec: C,
+) -> Result<ConsoleConnection<C::Item, InItem>, anyhow::Error>
+where
+    C: Decoder + Encoder<InItem> + Send + 'static,
+    C::Item: Send + 'static,
+    InItem: Send + 'static,
+    <C as Decoder>::Error: Send + std::fmt::Display,
+    <C as Encoder<InItem>>::Error: Send + std::fmt::Display,
+{
     #[cfg(unix)]
     stream.set_exclusive(false)?;
 
-    let (mut stream_reader, mut stream_writer) = split(stream);
+    let (mut stream_writer, mut stream_reader) = Framed::new(stream, codec).split();
 
-    let (log_writer, log_reader) = mpsc::unbounded_channel::<Bytes>();
-    let (command_writer, mut command_reader) = mpsc::unbounded_channel::<Bytes>();
+    let (log_writer, log_reader) = mpsc::unbounded_channel::<C::Item>();
+    let (command_writer, mut command_reader) = mpsc::unbounded_channel::<InItem>();
     let (error_writer, error_reader) = mpsc::unbounded_channel::<String>();
 
     tokio::spawn(async move {
-        let mut buffer = [0u8; 1024];
-
         loop {
             tokio::select! {
                 Some(cmd) = command_reader.recv() => {
-                    let _ = stream_writer.write(&cmd).await;
+                    if let Err(e) = stream_writer.send(cmd).await {
+                        let _ = error_writer.send(e.to_string());
+                    }
                 }
-                stream_read_result = stream_reader.read(&mut buffer) => {
-                    match stream_read_result {
-                        Ok(len) => {
-                            let _ = log_writer.send(Bytes::copy_from_slice(&buffer[..len]));
+                some_item = stream_reader.next() => {
+                    match some_item {
+                        Some(Ok(item)) => {
+                            let _ = log_writer.send(item);
                         }
-                        Err(e) => {
+                        Some(Err(e)) => {
                             let _ = error_writer.send(e.to_string());
                         }
+                        None => break,
                     }
                 }
             }
         }
-    })
-    .await?;
+    });
 
     Ok(ConsoleConnection {
-        log_reader,
+        log_reader: Mutex::new(log_reader),
         command_writer,
-        error_reader,
+        error_reader: Mutex::new(error_reader),
     })
 }
 
-impl ConsoleConnection {}
+impl<OutItem, InItem> ConsoleConnection<OutItem, InItem> {
+    pub async fn raw_read(&self) -> Result<OutItem, anyhow::Error> {
+        let mut reader = self.log_reader.lock().unwrap();
+
+        reader
+            .recv()
+            .await
+            .ok_or(anyhow::anyhow!("log_reader has closed"))
+    }
+
+    pub async fn raw_write(&self, item: InItem) -> Result<(), anyhow::Error> {
+        self.command_writer
+            .send(item)
+            .map_err(|_| anyhow::anyhow!("command_writer has closed"))
+    }
+}
