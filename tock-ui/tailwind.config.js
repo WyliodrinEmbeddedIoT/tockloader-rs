0b0fc982ae956febce98a827ab12d74dce344021
Adinusha <druicadenisa19@gmail.com>
2026-08-31 09:01:55 +0300

    fix: daisyui theme
    Signed-off-by: Adinusha <druicadenisa19@gmail.com>

--- tock-ui/tailwind.config.js
+++ tock-ui/tailwind.config.js
@@ -13,5 +13,35 @@ export default {
   },
   plugins: [
     require('daisyui'),
+    ],
+    
+  daisyui: {
+  themes: [
+    {
+      Tockloader: {
+        "primary": "#C2C2C2",
+        "primary-content": "#4C4C4C",
+        "secondary": "#4C4C4C",
+        "secondary-content": "#C2C2C2",
+        "accent": "#000000",
+        "accent-content": "#C2C2C2",
+        "neutral": "#C2C2C2",
+        "neutral-content": "#383838",
+        "base-100": "#383838",
+        "base-200": "#4C4C4C",
+        "base-300": "#2b2b2b",
+        "base-content": "#C2C2C2",
+        "info": "#3abff8",
+        "info-content": "#1a1a1a",
+        "success": "#1E453E",
+        "success-content": "#C2C2C2",
+        "warning": "#fbbd23",
+        "warning-content": "#1a1a1a",
+        "error": "#6F0000",
+        "error-content": "#C2C2C2",
+      },
+    },
   ],
+  darkTheme: "Tockloader",
+},  
 }
